7400e0b6c0337a8bba30a2098c94ba7290580f6c
Rahul Ghosh <rahulghosh111111@gmail.com>
2025-08-24 20:21:25 +0530

    Fix BubbleSort loop condition and improve output formatting for sorted array

--- SearchingAndSorting/BubbleSort.java
+++ SearchingAndSorting/BubbleSort.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class BubbleSort {
     static void sorting(int n,int arr[]){
         for(int j =0; j<n-1;j++){
-            for(int i=0;i<n-i-1;i++){
+            for(int i=0;i<n-j-1;i++){
                 if(arr[i]>arr[i+1]){
                     int temp = arr[i];
                     arr[i]=arr[i+1];
@@ -22,7 +22,7 @@ public class BubbleSort {
             arr[i]= sc.nextInt();
         }
         sorting(n,arr);
-        System.out.print("Sorted Array" );
+        System.out.print("Sorted Array: " );
         for(int i =0; i<n; i++){
             System.out.print(arr[i]+" ");
         }
