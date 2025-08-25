f159451f8cfae80f913a3289529c7ec69148f9c2
Rahul Ghosh <rahulghosh111111@gmail.com>
2025-08-26 01:02:00 +0530

    Implement LargestElement class to find and display the largest element in an array based on user input

--- ArrayQuestionsPractice/LargestElement.java
+++ ArrayQuestionsPractice/LargestElement.java
@@ -1,4 +1,29 @@
 package ArrayQuestionsPractice;
 
+import java.util.*;
+
 public class LargestElement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of the array: ");
+        int n = sc.nextInt();
+
+        if (n <= 0) {
+            System.out.println("Array size must be greater than 0.");
+            sc.close();
+            return;
+        }
+
+        List<Integer> list = new ArrayList<>();
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i < n; i++) {
+            list.add(sc.nextInt());
+        }
+
+        int max = Collections.max(list);
+        System.out.println("Largest element of the array is: " + max);
+
+        sc.close();
+    }
 }
