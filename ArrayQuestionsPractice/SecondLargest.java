104d43b7f7d4abcc73f13bb7b7c9efd56481b52d
Rahul Ghosh <rahulghosh111111@gmail.com>
2025-08-26 01:02:18 +0530

    Implement SecondLargest class to find and display the second largest element in an array based on user input

--- ArrayQuestionsPractice/SecondLargest.java
+++ ArrayQuestionsPractice/SecondLargest.java
@@ -1,4 +1,43 @@
 package ArrayQuestionsPractice;
+import java.util.*;
 
 public class SecondLargest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of the array: ");
+        int n = sc.nextInt();
+
+        if (n < 2) {
+            System.out.println("Array must contain at least two elements.");
+            sc.close();
+            return;
+        }
+
+        List<Integer> list = new ArrayList<>();
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i < n; i++) {
+            list.add(sc.nextInt());
+        }
+
+        int max = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int num : list) {
+            if (num > max) {
+                secondLargest = max;
+                max = num;
+            } else if (num > secondLargest && num != max) {
+                secondLargest = num;
+            }
+        }
+
+        if (secondLargest == Integer.MIN_VALUE) {
+            System.out.println("There is no second largest element.");
+        } else {
+            System.out.println("Second Largest element of the array is: " + secondLargest);
+        }
+
+        sc.close();
+    }
 }
